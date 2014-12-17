1dc2d63b90ac059808d085b5d17dd87da7346524
Bean <Bean.B.Deng@newegg.com>
2014-12-17 17:23:30 +0800

    change css/js files location

---
+++ HAOestMVCTemplate/HAOestMVCTemplate/Resources/_references.js
@@ -0,0 +1,3 @@
+/// <autosync enabled="true" />
+/// <reference path="../resources/thirdparty/bootstrap/3.3.1/js/bootstrap.js" />
+/// <reference path="../resources/thirdparty/jquery/2.1.1/jquery-2.1.1.js" />
